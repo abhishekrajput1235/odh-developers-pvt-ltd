719be1e2a782cb0ed5a73ad6fb0ee6d4c2d9466b
abhishekrajput1235 <anandraj60094@gmail.com>
2025-10-08 01:34:33 +0530

    Update UI and some content

--- main.py
+++ main.py
@@ -27,9 +27,22 @@ async def custom_http_exception_handler(request: Request, exc: StarletteHTTPExce
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+
+@app.get("/register", response_class=HTMLResponse)
+async def register(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
+@app.get("/odh-login", response_class=HTMLResponse)
+async def login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
 @app.get("/about", response_class=HTMLResponse)
 async def about(request: Request):
     return templates.TemplateResponse("about.html", {"request": request})
+
+@app.get('/more-about', response_class=HTMLResponse)
+async def moreabout(request:Request):
+    return templates.TemplateResponse("moreabout.html",{"request": request})
 
 @app.get("/services", response_class=HTMLResponse)
 async def about(request: Request):
@@ -50,29 +63,53 @@ async def about(request: Request):
     return templates.TemplateResponse("propertyDetails.html", {"request": request})
 
 @app.get("/odh-agencies", response_class=HTMLResponse)
-async def about(request: Request):
+async def agencies(request: Request):
     return templates.TemplateResponse("agencies.html", {"request": request})
 
 @app.get("/agencies-details", response_class=HTMLResponse)
-async def about(request: Request):
+async def agencies(request: Request):
     return templates.TemplateResponse("agenciesDetails.html", {"request": request})
 
 @app.get("/odh-blogs", response_class=HTMLResponse)
-async def about(request: Request):
+async def blogs(request: Request):
     return templates.TemplateResponse("/blog.html", {"request": request})
 
 @app.get("/odh-details-blog", response_class=HTMLResponse)
-async def about(request: Request):
+async def blogs(request: Request):
     return templates.TemplateResponse("blogDetails.html", {"request": request})
 
 @app.get("/odh-contact", response_class=HTMLResponse)
-async def about(request: Request):
+async def contact(request: Request):
     return templates.TemplateResponse("contactus.html", {"request": request})
 
 @app.get("/odh-team",response_class=HTMLResponse)
-async def about(request: Request):
+async def team(request: Request):
     return templates.TemplateResponse("team.html", {"request": request})
 
 @app.get("/career-with-odh", response_class=HTMLResponse)
-async def about(request: Request):
-    return templates.TemplateResponse("error.html", {"request": request})
+async def career(request: Request):
+    return templates.TemplateResponse("career.html", {"request": request})
+
+@app.get("/odh-developers-gallery", response_class=HTMLResponse)
+async def gallery(request: Request):
+    return templates.TemplateResponse("gallery.html", {"request": request})
+
+@app.get("/odh-faq", response_class=HTMLResponse)
+async def faq(request: Request):
+    return templates.TemplateResponse("faq.html", {"request": request})
+
+
+
+@app.get('/odh-terms-conditions', response_class=HTMLResponse)
+async def conditions(request:Request):
+    return templates.TemplateResponse("terms.html",{"request": request})
+
+@app.get('/odh-privacy-policy', response_class=HTMLResponse)
+async def privacy(request:Request):
+    return templates.TemplateResponse("privacy.html",{"request": request})
+
+
+@app.get("/disclaimer", response_class=HTMLResponse)
+async def disclaimer(request: Request):
+    return templates.TemplateResponse("disclaimer.html", {"request": request})
+
